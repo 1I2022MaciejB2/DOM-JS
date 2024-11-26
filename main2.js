506bc1be13b14d3e2ac9650e964142d327ede03e
Maciej Bazyl <1i2022maciejb2@gmail.com>
2024-11-26 09:35:04 +0100

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,5 @@
+document.getElementById("h1").innerHTML = "aa"
+document.getElementsByTagName("img")
+document.getElementsByClassName("h2")
+document.querySelector("h1").innerHTML = "AAA"
+document.querySelectorAll("h1").innerHTML = "QQQ"
