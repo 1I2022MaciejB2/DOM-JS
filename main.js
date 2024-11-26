42cf1c3b5db19a7d9142f4dec68886dc9833a6e3
Maciej Bazyl <1i2022maciejb2@gmail.com>
2024-11-26 08:45:58 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Dwie metody zrobienia tablicy z pobranych elementów, które są obiektem tablico-podobnym<br>
+
+// 1
+const imgs = Array.from(document.images);
+// 2
+const imgs2 = [...document.images];
